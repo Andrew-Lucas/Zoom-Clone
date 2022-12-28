0f2589f054049f3bd29c53fd68b3018b9ead3f32
Andrew Lucas <113585213+Andrew-Lucas@users.noreply.github.com>
2022-12-28 18:58:15 +0300

    introduction to WEBTCR

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,100 +1,79 @@
 const socket = io()
 
-const nickNameForm = document.querySelector(".nick-name_form")
-const nickName = nickNameForm.querySelector("input")
+const myVideo = document.getElementById("myVideo")
+const cameraDevices = document.getElementById("cameras")
+const muteBtn = document.getElementById("muteBtn")
+const cameraBtn = document.getElementById("disableCameraBtn")
 
-const enterChannelForm = document.querySelector(".enter-channel_form")
-const channelName = enterChannelForm.querySelector(".channel-name")
+let videoStream;
+let muted = false
+let disabledCamera = false
 
-const roomChannel = document.getElementById("welcome-channel")
-const enterMessageForm = document.querySelector(".enter-message_form")
-const messageInput = enterMessageForm.querySelector(".message-text")
-
-enterMessageForm.hidden = true
-let roomName;
-
-
-function saveNickName(submitEvent){
-  submitEvent.preventDefault()
-  socket.emit("save_nickname", nickName.value)
-  roomName = nickName.value
-  nickName.value = ""
-}
-nickNameForm.addEventListener("submit", saveNickName)
-socket.on("saved_nickname", (nickname)=>{
-  const userNickname = document.getElementById("user-nickname")
-  userNickname.innerText = nickname
-})
-
-function emitCompleted(){
-  enterChannelForm.hidden = true
-  nickNameForm.hidden = true
-  enterMessageForm.hidden = false
-  const chatName = document.querySelector(".chat-name")
-  chatName.innerText = `Room ${roomName}`
-}
-function handleEnterChannel(submitEvent){
-  submitEvent.preventDefault()
-  socket.emit("enter_channel", channelName.value, emitCompleted)
-  roomName = channelName.value
-  channelName.value = ""
-}
-
-function addMessage(message){
-/*   console.log(socket.nickname) */
-  const ul = roomChannel.children[1]
-  const li = document.createElement("li")
-  li.innerText = message
-  ul.appendChild(li)
-}
-socket.on("welcome", (nickname, newCount)=>{
-  const chatName = document.querySelector(".chat-name")
-  chatName.innerText = `Room ${roomName} (${newCount})`
-  console.log("Hello", nickname)
-  addMessage(`${nickname} joined this channel`)
-})
-socket.on("bye", (nickname, newCount)=>{
-  const chatName = document.querySelector(".chat-name")
-  chatName.innerText = `Room ${roomName} (${newCount})`
-  console.log("Bye", nickname)
-  addMessage(`${nickname} disconnected`)
-})
-
-enterChannelForm.addEventListener("submit", handleEnterChannel)
-
-
-
-function sendMessage(submitEvent){
-  submitEvent.preventDefault()
-  const msgText = messageInput.value
-  socket.emit("send_message", messageInput.value, roomName, ()=>{
-    addMessage(`You: ${msgText}`)
-  })
-  messageInput.value = ""
-}
-
-socket.on("new_message", addMessage)
-
-socket.on("room_change", (rooms)=>{
-  console.log(rooms)
-  const roomsUl = enterChannelForm.querySelector("ul")
-  roomsUl.innerHTML = ""
-  if(rooms.length == 0){
-    return;
+async function getCameras(){
+  try{
+    const devices = await navigator.mediaDevices.enumerateDevices()
+    const cameras = devices.filter(device => device.kind === "videoinput")
+    const currentCamera = videoStream.getVideoTracks()[0]
+    cameras.forEach((camera)=>{
+      const optDevice = document.createElement("option")
+      optDevice.value = camera.deviceId
+      optDevice.innerText = camera.label
+      cameraDevices.appendChild(optDevice)
+      if(currentCamera.label === camera.label){
+        optDevice.selected = true
+      }
+    })
+  } catch(err){
+    console.log(err)
   }
-  rooms.forEach((room)=>{
-/*     const exists = rooms.find((eachRoom)=> eachRoom===room)
-    if(exists){
-      console.log(exists, "already exists")
-      return
-    }
-    console.log(exists) */
-    const roomLi = document.createElement("li")
-    roomLi.innerText = room
-    roomsUl.appendChild(roomLi)
-/*     console.log(roomsUl)
-    console.log(roomLi) */
-  })
-})
+}
 
-enterMessageForm.addEventListener("submit", sendMessage)
+async function getMedia(deviceId){
+  const initialConstrains = {
+    audio: true,
+    video: {facingMode: "user"}
+  }
+  const cameraConstrains = {
+    audio: true,
+    video:{deviceId: {exact: deviceId}}
+  }
+  try{
+    videoStream = await navigator.mediaDevices.getUserMedia(deviceId ? cameraConstrains : initialConstrains)
+    myVideo.srcObject = videoStream
+    if(!deviceId){
+      await getCameras()
+    }
+  } catch(err){
+    console.log(err)
+  }
+}
+getMedia()
+getCameras()
+
+function handleMuteClick(){
+  videoStream.getAudioTracks().forEach(track => track.enabled = !track.enabled)
+  if (muted) {
+    muted = false
+  } else {
+    muted = true
+  }
+  muteBtn.innerText = muted ? 'Unmute' : 'Mute'
+}
+
+function handleCameraClick(){
+  videoStream.getVideoTracks().forEach(track => track.enabled = !track.enabled)
+  if (!disabledCamera) {
+    disabledCamera = true
+  } else {
+    disabledCamera = false
+  }
+  cameraBtn.innerText = disabledCamera ? "Enable Camera" : "Disable Camera"
+}
+
+async function handleCameraChange(){
+  await getMedia(cameraDevices.value)
+}
+
+cameraDevices.addEventListener("input", handleCameraChange)
+muteBtn.addEventListener("click", handleMuteClick)
+cameraBtn.addEventListener("click", handleCameraClick)
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http"
-import {Server} from "socket.io"
+import SocketIO from "socket.io"
 import {instrument} from "@socket.io/admin-ui"
 import express from "express"
 import { Socket } from "net"
@@ -19,93 +19,6 @@ app.get("/*", (req, res)=>{
 })
 
 const httpServer = http.createServer(app)
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true
-  }
-})
-
-instrument(wsServer, {
-  auth: false,
-})
-
-wsServer.on("connection", socket=>{
-  socket["nickname"] = "Anonymous"
-  socket.onAny((socketEvent)=>{
-    console.log(`Socket Event: ${socketEvent}`)
-    console.log(socket.rooms)
-  })
-
-  socket.on("save_nickname", (nickname)=>{
-    socket["nickname"] = nickname
-    socket.emit("saved_nickname", nickname)
-  })
-
-  function publicRooms(){
-    const {sids, rooms} = wsServer.sockets.adapter
-/*     console.log("SIDS::", sids, "ROOMS::", rooms) */
-    const publicRooms = []
-    rooms.forEach((_, key)=>{
-      if(sids.get(key) === undefined){
-        publicRooms.push(key)
-      }
-    })
-    console.log(publicRooms)
-    return publicRooms
-  }
-
-  function countRoom(roomName){
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size
-  }
-
-  socket.on("enter_channel", (roomName, done)=>{
-    socket.join(roomName)
-    done() 
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName))
-    wsServer.sockets.emit("room_change", publicRooms())
-  })
-
-  socket.on("disconnecting", ()=>{
-    socket.rooms.forEach((room)=> socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1))
-  })
-
-  socket.on("disconnect", ()=>{
-    wsServer.sockets.emit("room_change", publicRooms()) 
-  })
-
-  socket.on("send_message", (newMessage, room, done)=>{
-    done()
-    console.log(newMessage)
-    socket.to(room).emit("new_message", `${socket.nickname}: ${newMessage}`)
-  })
-})
-
-
-
-/* const wss = new WebSocket.Server({server})
-
- let socketArr = []
-wss.on("connection", (socket)=>{
-  console.log("Connected to the browser ✓")
-  socketArr.push(socket)
-  socket["nickname"] = "Anonymous"
-  socket.on("open", ()=>{
-  })
-  socket.on("close", ()=>{
-    console.log("Disconnected from the browser💀")
-  })
-  socket.addEventListener("message", (message)=> {
-    const parse = JSON.parse(message.data)
-    console.log(parse)
-    switch (parse.type){
-      case "sms":
-        socketArr.forEach((eachSocket)=> eachSocket.send(`${socket.nickname}: ${parse.value}`))
-      case "nickname":
-        socket["nickname"] = parse.value
-    }
-  })
-}) */
-
+const wsServer = SocketIO(httpServer)
 
 httpServer.listen(PORT, () =>console.log(`✅Server listening from http://localhost:${PORT}`))
